8c471d9c4b8c8c0a1b8fd6b0f43c29387d7f8b18
Muhammet Emre Numanoğlu <muhammedemrenumanoglu155@gmail.com>
2026-04-18 18:10:06 +0300

    OutsideClickHandler added

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,32 +1,65 @@
-import React from 'react'
-import Logo from "../ui/Logo.jsx"
-import { FaUserAlt,FaShoppingCart,FaSearch } from "react-icons/fa";
+import React, { useState } from "react";
+import Logo from "../ui/Logo.jsx";
+import { FaUserAlt, FaShoppingCart, FaSearch } from "react-icons/fa";
+import OutsideClickHandler from "react-outside-click-handler";
+import Title from "../ui/Title.jsx";
 
 const Header = () => {
+  const [isSearchModal, setIsSearchModal] = useState(false);
   return (
-    <div className='h-[5.5rem] bg-secondary '>    
-<div className='container text-white mx-auto flex justify-between items-center h-full'>
-        <div><Logo/></div>
-    <div>
-        <nav>
-            <ul className='flex gap-x-1'>
-                <li className='px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer transition-all'><a href="">Home</a></li>
-                <li className='px-[5px] py-[10px] uppercase'><a href="">Menu</a></li>
-                <li className='px-[5px] py-[10px] uppercase'><a href="">About</a></li>
-                <li className='px-[5px] py-[10px] uppercase'><a href="">Book Table</a></li>
-
+    <div className="h-[5.5rem] bg-secondary ">
+      <div className="container text-white mx-auto flex justify-between items-center h-full">
+        <div>
+          <Logo />
+        </div>
+        <div>
+          <nav>
+            <ul className="flex gap-x-1">
+              <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer transition-all">
+                <a href="">Home</a>
+              </li>
+              <li className="px-[5px] py-[10px] uppercase">
+                <a href="">Menu</a>
+              </li>
+              <li className="px-[5px] py-[10px] uppercase">
+                <a href="">About</a>
+              </li>
+              <li className="px-[5px] py-[10px] uppercase">
+                <a href="">Book Table</a>
+              </li>
             </ul>
-        </nav>
+          </nav>
+        </div>
+        <div className="flex gap-x-4 ">
+          <a href="#">
+            <FaUserAlt className="hover:text-primary transition-all" />
+          </a>
+          <a href="#">
+            <FaShoppingCart className="hover:text-primary transition-all" />
+          </a>
+          <button onClick={() => setIsSearchModal(true)}>
+            <FaSearch className="hover:text-primary transition-all" />
+          </button>
+          <a href="">
+            <button className="btn-primary">Order Online</button>
+          </a>
+        </div>
+      </div>
+      {isSearchModal && (
+        <div>
+          <OutsideClickHandler onOutsideClick={() => setIsSearchModal(false)}>
+            <div className="">
+              {" "}
+              <Title>Title</Title>{" "}
+            </div>
+            <div>
+              <div>Search Modal</div>
+            </div>
+          </OutsideClickHandler>
+        </div>
+      )}
     </div>
-    <div className='flex gap-x-4 '>
-    <a href="#"><FaUserAlt /></a>
-    <a href="#"><FaShoppingCart/></a>
-    <a href="#"><FaSearch /></a>
-    <a href=""><button className='btn-primary'>Order Online</button></a>
-    </div>
-</div>
-</div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
