e5a81cf854859e6497235f6aa80c62716a7a5b37
Muhammet Emre Numanoğlu <muhammedemrenumanoglu155@gmail.com>
2026-05-02 18:25:29 +0300

    Customer Slider Added

--- components/customer/Customer.jsx
+++ components/customer/Customer.jsx
@@ -1,17 +1,65 @@
 import React from "react";
 import Title from "../ui/Title";
 import CustomerItem from "./CustomerItem";
+import Slider from "react-slick";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
-const Customer = () => {
+const Customers = () => {
+  function NextBtn({ onClick }) {
+    return (
+      <button
+        className="absolute -bottom-12 left-1/2 bg-primary flex items-center justify-center w-10 h-10 rounded-full text-white"
+        onClick={onClick}
+      >
+        <IoIosArrowForward />
+      </button>
+    );
+  }
+
+  function PrevBtn({ onClick }) {
+    return (
+      <button
+        className="absolute -bottom-12 right-1/2 bg-primary flex items-center justify-center w-10 h-10 rounded-full text-white mr-2"
+        onClick={onClick}
+      >
+        <IoIosArrowBack />
+      </button>
+    );
+  }
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    arrows: true,
+    nextArrow: <NextBtn></NextBtn>,
+    prevArrow: <PrevBtn></PrevBtn>,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="container my-20 mx-auto">
-      <Title addClass={" text-center"}>What Our Customer Says</Title>
-      <div className="flex gap-x-10">
-        <CustomerItem imgSrc={"/images/client1.jpg"}/>
-        <CustomerItem imgSrc={"/images/client2.jpg"}/>
-      </div>
+    <div className="container mx-auto my-20">
+      <Title addClass="text-[40px] text-center">What Says Our Customers</Title>
+      <Slider {...settings}>
+        <CustomerItem imgSrc="/images/client1.jpg" />
+        <CustomerItem imgSrc="/images/client2.jpg" />
+        <CustomerItem imgSrc="/images/client1.jpg" />
+        <CustomerItem imgSrc="/images/client2.jpg" />
+      </Slider>
     </div>
   );
 };
 
-export default Customer;
+export default Customers;
