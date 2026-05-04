f5eae9f4df9ede35bb232054c5cbd801ad44c060
Muhammet Emre Numanoğlu <muhammedemrenumanoglu155@gmail.com>
2026-05-04 17:21:00 +0300

    Cart Page Total Added

---
+++ components/layout/Header.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { FaUserAlt, FaShoppingCart, FaSearch } from "react-icons/fa";
+import Logo from "../ui/Logo";
+import Search from "../ui/Search";
+import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const Header = () => {
+  const [isSearchModal, setIsSearchModal] = useState(false);
+  const [isMenuModal, setIsMenuModal] = useState(false);
+
+  const router = useRouter();
+
+  return (
+    <div
+      className={`h-[5.5rem] z-50 relative ${
+        router.asPath === "/" ? "bg-transparent" : "bg-secondary"
+      }`}
+    >
+      <div className="container mx-auto text-white flex justify-between items-center h-full">
+        <Logo />
+
+        {/* NAV */}
+        <nav
+          className={`sm:static absolute top-0 left-0 w-full sm:w-auto sm:h-auto h-screen sm:bg-transparent bg-white sm:text-white text-black ${
+            isMenuModal
+              ? "grid place-content-center"
+              : "hidden sm:flex"
+          }`}
+        >
+          <ul className="flex gap-x-2 sm:flex-row flex-col items-center">
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <Link href="/menu">Menu</Link>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <Link href="/about">About</Link>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <Link href="/reservation">Reservation</Link>
+            </li>
+          </ul>
+
+          {isMenuModal && (
+            <button
+              className="absolute top-4 right-4 z-50"
+              onClick={() => setIsMenuModal(false)}
+            >
+              <GiCancel size={25} />
+            </button>
+          )}
+        </nav>
+
+        {/* ICONS */}
+        <div className="flex gap-x-4 items-center">
+          <Link href="/auth/login">
+            <FaUserAlt className="hover:text-primary transition-all cursor-pointer" />
+          </Link>
+          <Link href="/cart">
+            <FaShoppingCart className="hover:text-primary transition-all cursor-pointer" />
+          </Link>
+          <button onClick={() => setIsSearchModal(true)}>
+            <FaSearch className="hover:text-primary transition-all cursor-pointer" />
+          </button>
+          <Link href="/order">
+            <button className="btn-primary md:inline-block hidden">
+              Order Online
+            </button>
+          </Link>
+          <button
+            className="sm:hidden inline-block"
+            onClick={() => setIsMenuModal(true)}
+          >
+            <GiHamburgerMenu className="text-xl hover:text-primary transition-all" />
+          </button>
+        </div>
+      </div>
+
+      {isSearchModal && <Search setIsSearchModal={setIsSearchModal} />}
+    </div>
+  );
+};
+
+export default Header;
