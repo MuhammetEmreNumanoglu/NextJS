86940fc6c4327312a07fedb31e00fb2f20f2805a
Muhammet Emre Numanoğlu <muhammedemrenumanoglu155@gmail.com>
2026-05-08 17:20:41 +0300

    Profile Responsive Added

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import React from "react";
 import Input from "../../components/form/Input";
 import Title from "../../components/ui/Title";
 import { useFormik } from "formik";
 import { profileSchema } from "../../schema/profile";
 
-const Profile = () => {
+const Account = () => {
   const onSubmit = async (values, actions) => {
     await new Promise((resolve) => setTimeout(resolve, 4000));
     actions.resetForm();
@@ -80,53 +80,21 @@ const Profile = () => {
     },
   ];
   return (
-    <div className="flex px-10 min-h-[calc(100vh_-_433px)]">
-      <div className="w-80 flex-shrink-0">
-        <div className="relative flex flex-col items-center px-10 py-5 border border-b-0">
-          <Image
-            src="/images/client2.jpg"
-            alt=""
-            width={100}
-            height={100}
-            className="rounded-full"
+    <form className="lg:p-8 flex-1 lg:mt-0 mt-5">
+      <Title addClass="text-[40px]">Account Settings</Title>
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-4">
+        {inputs.map((input) => (
+          <Input
+            key="input.id"
+            {...input}
+            onBlur={handleBlur}
+            onChange={handleChange}
           />
-          <b className="text-2xl mt-1">John Doe</b>
-        </div>
-        <ul className="text-center font-semibold">
-          <li className="border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all">
-            <i className="fa fa-home"></i>
-            <button className="ml-1 ">Account</button>
-          </li>
-          <li className="border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all">
-            <i className="fa fa-key"></i>
-            <button className="ml-1">Password</button>
-          </li>
-          <li className="border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all">
-            <i className="fa fa-motorcycle"></i>
-            <button className="ml-1">Orders</button>
-          </li>
-          <li className="border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all">
-            <i className="fa fa-sign-out"></i>
-            <button className="ml-1">Exit</button>
-          </li>
-        </ul>
+        ))}
       </div>
-      <form className="p-8 flex-1">
-        <Title addClass="text-[40px]">Account Settings</Title>
-        <div className="grid grid-cols-2 gap-4 mt-4">
-          {inputs.map((input) => (
-            <Input
-              key="input.id"
-              {...input}
-              onBlur={handleBlur}
-              onChange={handleChange}
-            />
-          ))}
-        </div>
-        <button className="btn-primary mt-4">Update</button>
-      </form>
-    </div>
+      <button className="btn-primary mt-4">Update</button>
+    </form>
   );
 };
 
-export default Profile;
+export default Account;
