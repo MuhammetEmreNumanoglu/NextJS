ea04d03c1d3138fb9729f10624d089eea5d29535
Muhammet Emre Numanoğlu <github-mail-adresin@example.com>
2026-05-12 17:14:21 +0300

    User GET And POST Operations

--- util/dbConnect.js
+++ util/dbConnect.js
@@ -8,7 +8,11 @@ if (!MONGODB_URI) {
   );
 }
 
-
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
 let cached = global.mongoose;
 
 if (!cached) {
