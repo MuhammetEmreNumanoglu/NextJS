5be329cd5319ca51bab37acb9a00769d25f79d10
Muhammet Emre Numanoglu <muhammedemrenumanoglu155@gmail.com>
2026-07-04 17:12:12 +0300

    Layout Usage

---
+++ src/app/blog/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function BlogLayout({children}) {
+  return (
+    <div>
+        Blog Layout
+
+        {children}
+    
+    </div>
+  )
+}
--- src/app/suv/[...slug]/page.js
+++ src/app/suv/[...slug]/page.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 export default async function SuvChildPage({params,searchParams}) {
     const {slug} = await params;
-    console.log(await searchParams)
+    const filter = await searchParams;
+    console.log(filter)
     console.log(slug);
   return (
     <div>2
       SuvChildPage
-    </div>
+    </div> 
   )
 }
